afd5f0008ca075fcccb4825d19c3eef8576ed626
Ch. König <me@kurabloodlust.eu>
2018-09-22 22:16:56 +0200

    godoc fixes

--- reverse.go
+++ reverse.go
@@ -18,28 +18,28 @@ func Clear() {
 	}
 }
 
-// Adds url to store
+// Add url to store
 func Add(urlName, urlAddr string, params ...string) string {
 	return Urls.MustAdd(urlName, urlAddr, params...)
 }
 
-// Adds url with concat group, but returns just urlAddr
+// AddGr url with concat group, but returns just urlAddr
 func AddGr(urlName, group, urlAddr string, params ...string) string {
 	return Urls.MustAddGr(urlName, group, urlAddr, params...)
 }
 
-// Reverse url by name
+// Rev url by name
 func Rev(urlName string, params ...string) string {
 	return Urls.MustReverse(urlName, params...)
 }
 
-// Gets raw url by name
+// Get raw url by name
 func Get(urlName string) string {
 	return Urls.Get(urlName)
 }
 
-// Gets saved all urls
-func GetAllUrls() map[string]string {
+// GetAllURLs saved all urls
+func GetAllURLs() map[string]string {
 	out := map[string]string{}
 	for key, value := range Urls.store {
 		out[key] = value.url
@@ -47,7 +47,7 @@ func GetAllUrls() map[string]string {
 	return out
 }
 
-// Gets all params
+// GetAllParams all params
 func GetAllParams() map[string][]string {
 	out := map[string][]string{}
 	for key, value := range Urls.store {
@@ -65,12 +65,12 @@ type urlStore struct {
 	store map[string]url
 }
 
-// Adds a Url to the Store
+// Add a Url to the Store
 func (us *urlStore) Add(urlName, urlAddr string, params ...string) (string, error) {
 	return us.AddGr(urlName, "", urlAddr, params...)
 }
 
-// Adds a Url and panics if error
+// MustAdd a Url and panics if error
 func (us urlStore) MustAdd(urlName, urlAddr string, params ...string) string {
 	addr, err := us.Add(urlName, urlAddr, params...)
 	if err != nil {
@@ -79,7 +79,7 @@ func (us urlStore) MustAdd(urlName, urlAddr string, params ...string) string {
 	return addr
 }
 
-// Adds with group prefix
+// AddGr with group prefix
 func (us *urlStore) AddGr(urlName, group, urlAddr string, params ...string) (string, error) {
 	if _, ok := us.store[urlName]; ok {
 		return "", errors.New("Url already exists. Try to use .Get() method.")
@@ -90,7 +90,7 @@ func (us *urlStore) AddGr(urlName, group, urlAddr string, params ...string) (str
 	return urlAddr, nil
 }
 
-// Adds a Url with group prefix
+// MustAddGr a Url with group prefix
 func (us urlStore) MustAddGr(urlName, group, urlAddr string, params ...string) string {
 	addr, err := us.AddGr(urlName, group, urlAddr, params...)
 	if err != nil {
@@ -99,12 +99,12 @@ func (us urlStore) MustAddGr(urlName, group, urlAddr string, params ...string) s
 	return addr
 }
 
-// Gets raw url string
+// Get raw url string
 func (us urlStore) Get(urlName string) string {
 	return us.store[urlName].url
 }
 
-// Gets reversed url
+// Reverse url
 func (us urlStore) Reverse(urlName string, params ...string) (string, error) {
 	if len(params) != len(us.store[urlName].params) {
 		return "", errors.New("Bad Url Reverse: mismatch params for URL: " + urlName)
@@ -116,7 +116,7 @@ func (us urlStore) Reverse(urlName string, params ...string) (string, error) {
 	return res, nil
 }
 
-// Gets reversed url and panics if error
+// MustReverse url and panics if error
 func (us urlStore) MustReverse(urlName string, params ...string) string {
 	res, err := us.Reverse(urlName, params...)
 	if err != nil {
@@ -125,7 +125,7 @@ func (us urlStore) MustReverse(urlName string, params ...string) string {
 	return res
 }
 
-// Reverse url, but returns empty string in case of error
+// Rev url, but returns empty string in case of error
 func (us urlStore) Rev(urlName string, params ...string) string {
 	return us.MustReverse(urlName, params...)
 }
