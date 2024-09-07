2a44a54a7ffb29ae8046fcb9e3cdbc9095b3f5d8
Alexander Kharchenko <morontt@yandex.ru>
2024-09-07 17:01:01 +0300

    start refactoring

--- reverse_test.go
+++ reverse_test.go
@@ -7,50 +7,45 @@ import (
 
 func TestReverse(t *testing.T) {
 	showError := func(info string) {
-		t.Error(fmt.Sprintf("Error: %s. urlStore: %s", info, Urls))
+		t.Error(fmt.Sprintf("Error: %s. urlStore: %s", info, routes))
 	}
 
-	if Urls.MustAdd("firstUrl", "/first") != "/first" {
+	if routes.mustAdd("firstUrl", "/first") != "/first" {
 		showError("0")
 	}
 
-	if Urls.MustAdd("helloUrl", "/hello/:p1:p2", "1", "2") != "/hello/:p1:p2" {
+	if routes.mustAdd("helloUrl", "/hello/:p1:p2", "1", "2") != "/hello/:p1:p2" {
 		showError("0-1")
 	}
 
-	Urls.MustAdd("secondUrl", "/second/:param/:param2", ":param", ":param2")
+	routes.mustAdd("secondUrl", "/second/:param/:param2", ":param", ":param2")
 
 	// re := regexp.MustCompile("^/comment/(?P<id>\d+)$")
-	Urls.MustAdd("thirdUrl", "/comment/:p1", ":p1")
+	routes.mustAdd("thirdUrl", "/comment/:p1", ":p1")
 
-	if Urls.getParam("helloUrl", 1) != "2" {
+	if routes.getParam("helloUrl", 1) != "2" {
 		showError("1")
 	}
 
-	if Urls.Get("helloUrl") != "/hello/:p1:p2" {
-		showError("2")
-	}
-
-	if Urls.getParam("secondUrl", 0) != ":param" {
+	if routes.getParam("secondUrl", 0) != ":param" {
 		showError("3")
 	}
 
-	if Urls.MustReverse("firstUrl") != "/first" {
+	if routes.mustReverse("firstUrl") != "/first" {
 		showError("4")
 	}
 
-	if Urls.MustReverse("secondUrl", "123", "ABC") != "/second/123/ABC" {
+	if routes.mustReverse("secondUrl", "123", "ABC") != "/second/123/ABC" {
 		showError("5")
 	}
 
-	if Urls.MustReverse("thirdUrl", "123") != "/comment/123" {
-		t.Error(Urls.Reverse("thirdUrl", "123"))
+	if routes.mustReverse("thirdUrl", "123") != "/comment/123" {
+		t.Error(routes.reverse("thirdUrl", "123"))
 		showError("6")
 	}
 
-	Clear()
-	if len(Urls.store) != 0 {
+	clearRoutes()
+	if len(routes.store) != 0 {
 		showError("7")
 	}
-
 }
